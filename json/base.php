07c400eacfe3104f252e993c485c07847f496d65
Mikael-Salasca <mikael.salasca@etu.univ-amu.fr>
2018-03-11 14:52:46 +0100

    mysqli pour se co //bug avec pdo

--- json/base.php
+++ json/base.php
@@ -6,15 +6,21 @@
  * Time: 15:29
  */
 
-$user = '130266';
-$pass = '123456789';
-$dsn = 'mysql:host= mysql-mikaelsalasca.alwaysdata.net;dbname=mikaelsalasca_post-it';
+//$user = '130266';
+//$pass = '123456789';
+//$dsn = 'mysql:host= mysql-mikaelsalasca.alwaysdata.net;dbname=mikaelsalasca_post-it';
+//
+////try {
+//    echo $user, $pass, $dsn;
+//    $db = new PDO($dsn, $user, $pass);
+//} catch (PDOException $e) {
+//    print "Erreur !: " . $e->getMessage() . "<br/>";
+//    die();
+//}
 
-try {
-    echo $user, $pass, $dsn;
-    $db = new PDO($dsn, $user, $pass);
-} catch (PDOException $e) {
-    print "Erreur !: " . $e->getMessage() . "<br/>";
-    die();
-}
-?>
+
+$dbLink = mysqli_connect('mysql-mikaelsalasca.alwaysdata.net', '130266', '123456789')
+or die('Erreur de connexion au serveur : ' . mysqli_connect_error());
+mysqli_select_db($dbLink , 'mikaelsalasca_post-it')
+or die('Erreur dans la sélection de la base : ' . mysqli_error($dbLink)
+);
